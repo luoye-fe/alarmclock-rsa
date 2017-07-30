420cc3fa326291554cc3ac4d8db74b47658b383f
luoye <luoyefe@gmail.com>
2017-07-30 21:18:04 +0800

    update log func

--- logger.js
+++ logger.js
@@ -9,7 +9,7 @@ const exportObj = {};
 
 proxy.forEach(item => {
 	exportObj[item] = function funcName(...args) {
-		return console[item].call(this, chalk.white(item) + ' ' + chalk.bold.green((new Date()).toLocaleString()) + sep + args[0], args[1] || '');
+		return console[item].call(this, chalk.bold.green((new Date()).toLocaleString()) + sep + args[0], args[1] || '');
 	};
 });
 
